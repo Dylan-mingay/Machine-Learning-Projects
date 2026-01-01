cce3c7048c202ab5f62a5bfb7ab17f8cacea3cb3
Dylan-mingay <117301359+Dylan-mingay@users.noreply.github.com>
2026-01-01 22:14:07 +0000

    Worked on code for finding the weighted impurty based off of gini impurity and entropy

--- second_ml_project/src/information_gain.py
+++ second_ml_project/src/information_gain.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+
+criterion_function = {'gini': gini_impurity, 'entropy': entropy}
 # information gain calculation works out the entropy of a set of labels
 def information_gain(pos_fractions, labels):
     """
@@ -28,9 +30,26 @@ def information_gain(pos_fractions, labels):
     # Count the occurrences of each label
     counts = np.unique(labels, return_counts=True)[1]
     fraction = counts / len(labels)
+
+    # Calculate impurity based on the label distribution using entropy formula
     impurity = -np.sum(fraction * np.log2(fraction + 1e-9))
 
     return impurity
+
+def weighted_impurity(groups, criterion='gini'):
+    '''
+    Docstring for weighted_impurity 
+    Weighted impurity of a split given the groups and the criterion after a split.
+    
+    :param groups: Description
+    :param citerion: Description
+    :return: weighted impurity
+    '''
+    total = sum(len(group) for group in groups)
+    weighted_sum = 0.0
+    for group in groups:
+        weighted_sum += len(group) / float(total) * criterion_function[criterion](group)
+    return weighted_sum
 
 pos_fractions = np.linspace(0.001, 0., 1000)
 
@@ -40,3 +59,4 @@ print(information_gain(pos_fractions, [1, 1, 1, 1, 1]))
 print(information_gain(pos_fractions, [0, 0, 0, 0, 0]))
 print(information_gain(pos_fractions, [1, 0, 1, 0, 1, 0]))
 print(information_gain(pos_fractions, [1, 1, 1, 0, 0, 0, 1, 0, 1]))
+
